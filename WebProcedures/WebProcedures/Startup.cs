7a151e90cc94272a0d6cce33a06aa96de5e89b8a
Enivaldo Queiroz <enivaldo.jecq@gmail.com>
2023-06-13 23:14:30 -0300

    Implemetação da controller Produto e a views conforme Model Produto

--- WebProcedures/WebProcedures/Startup.cs
+++ WebProcedures/WebProcedures/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebProcedures.Models;
 
 namespace WebProcedures
 {
@@ -24,6 +26,10 @@ namespace WebProcedures
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            string conexao = Configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<Contexto>(options => options.UseSqlServer(conexao));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
